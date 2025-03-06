28a86b1c2d0534a6f40845e5b1f31506d8d48616
Mickael <>
2025-03-06 11:55:42 +0900

    add menu

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type Word = {
+  id: string;
+  text: string;
+  lang: string;
+  image: string;
+  category: string;
+};
+
+export type Category = {
+  id: string;
+  name: string;
+  icon: string;
+}; 
