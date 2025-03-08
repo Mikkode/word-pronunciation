2f571438244aaac344ddaf6c4130f91834cc55ab
Mickael <>
2025-03-09 00:28:44 +0900

    card1

--- src/app/craft/page.tsx
+++ src/app/craft/page.tsx
@@ -4,19 +4,19 @@ import Content from "@/components/Content";
 
 export default function CraftPage() {
   // Couleurs pour les cartes
-  const colors = ["#ffffff", "#f8f9fa", "#e9ecef", "#dee2e6", "#ced4da"];
+  const colors = ["#ff56ac", "#56ebff", "#a057ff", "#ffa726"];
 
   // Données pour les cartes
   const cards = [
-    { id: 1, image: "/cat.png", title: "Chat", color: colors[0] },
-    { id: 2, image: "/cat.png", title: "Minou", color: colors[1] },
-    { id: 3, image: "/cat.png", title: "Félin", color: colors[2] },
-    { id: 4, image: "/cat.png", title: "Matou", color: colors[3] },
-    { id: 5, image: "/cat.png", title: "Chaton", color: colors[4] },
-    { id: 6, image: "/cat.png", title: "Tigre", color: colors[0] },
-    { id: 7, image: "/cat.png", title: "Lion", color: colors[1] },
-    { id: 8, image: "/cat.png", title: "Panthère", color: colors[2] },
-    { id: 9, image: "/cat.png", title: "Jaguar", color: colors[3] },
+    { id: 1, image: "/cat.png", title: "Chat" },
+    { id: 2, image: "/IMG_1163.jpg", title: "Minou" },
+    { id: 3, image: "/arc.png", title: "Félin" },
+    { id: 4, image: "/cat.png", title: "Matou" },
+    { id: 5, image: "/cat.png", title: "Chaton" },
+    { id: 6, image: "/cat.png", title: "Tigre" },
+    { id: 7, image: "/cat.png", title: "Lion" },
+    { id: 8, image: "/cat.png", title: "Panthère" },
+    { id: 9, image: "/cat.png", title: "Jaguar" },
   ];
 
   return (
@@ -26,15 +26,14 @@ export default function CraftPage() {
         className="text-6xl font-extrabold mb-8 text-center font-kg-red-hands"
       />
       <Content>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {cards.map((card) => (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-16">
+          {cards.map((card, index) => (
             <Card1
               key={card.id}
               image={card.image}
               title={card.title}
-              color={card.color}
-              imageSize="contain"
-              className="w-full aspect-square"
+              color1={colors[index % colors.length]}
+              color2="white"
             />
           ))}
         </div>
